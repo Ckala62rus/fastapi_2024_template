f4b4389c20314478707d78c75035dfb83decafc3
Ckala <agr.akyla@mail.ru>
2024-07-10 11:01:53 +0300

    (feature) work on DockerFile and run application with debugger.

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import uvicorn
 
 from common.log import log
-# from backend.common.log import log
 from core.register import register_app
 
 app = register_app()
@@ -13,13 +12,12 @@ app = register_app()
 def read_root():
     log.info("testing logging info")
     log.error("testing logging with error2")
-    a = 1 + 30
-    return {"Hello": "World123"}
+    a = 2 + 30
+    return {"Hello": f"World123 {a}"}
 
 
 if __name__ == '__main__':
     try:
         uvicorn.run(app=f'{Path(__file__).stem}:app', reload=True, port=5001)
-        # uvicorn.run(app=app, reload=True, port=5001)
     except Exception as e:
         raise e
