3d91fe0949998ab340d9344c1bed2c4a6a5c1a0f
Ckala62rus <agr.akyla@mail.ru>
2024-07-15 13:21:17 +0300

    (feature) improve db connector.

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,18 +1,40 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+import sys
+from typing import Annotated
 
+from fastapi import Depends
+from sqlalchemy import create_engine, URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+
+from common.log import log
 from core.config import settings
 
-async_engine = create_async_engine(
-    # postgresql+asyncpg проверить
-    url=settings.SQLALCHEMY_DATABASE_URL,
-    # url="postgresql+psycopg://pguser:000000@localhost:5432/mydb",
-    echo=True,
-    # pool_size=5,
-    # max_overflow=10
-)
-
-SessionLocal = async_sessionmaker(async_engine)
-Engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
+
+def create_engine_and_session(url: str | URL):
+    try:
+        # Core database
+        engine = create_async_engine(url, echo=True, future=True, pool_pre_ping=True)
+        # log.success('success connect to database')
+    except Exception as e:
+        log.error('❌ Error to connect database {}', e)
+        sys.exit()
+    else:
+        db_session = async_sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+        return engine, db_session
+
+async_engine, async_db_session = create_engine_and_session(settings.SQLALCHEMY_DATABASE_URL)
+
+async def get_db() -> AsyncSession:
+    """session generator"""
+    session = async_db_session()
+    try:
+        yield session
+    except Exception as se:
+        await session.rollback()
+        raise se
+    finally:
+        await session.close()
+
+# Session Annotated
+CurrentSession = Annotated[AsyncSession, Depends(get_db)]
