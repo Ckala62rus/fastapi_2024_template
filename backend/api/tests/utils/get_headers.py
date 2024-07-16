f8fbc067e74d29700d7abb031fa91a99c8dfebbd
Ckala <agr.akyla@mail.ru>
2024-07-16 15:57:35 +0300

    (feature) add utils get_headers for tests.

---
+++ backend/api/tests/utils/get_headers.py
@@ -0,0 +1,18 @@
+from starlette.testclient import TestClient
+
+from core.config import settings
+
+
+def get_token_headers(client: TestClient, email: str, password: str) -> dict[str, str]:
+    data = {
+        'email': email,
+        'password': password,
+    }
+    response = client.post(f'{settings.API_V1_STR}/user/login', json=data)
+
+    response.raise_for_status()
+
+    token_type = response.json()['data']['token_type']
+    access_token = response.json()['data']['access_token']
+    headers = {'Authorization': f'{token_type} {access_token}'}
+    return headers
