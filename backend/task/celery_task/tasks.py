084c6cd5db81ee56713d23292e9502c94f1acdf2
Ckala <agr.akyla@mail.ru>
2024-07-19 08:29:00 +0300

    (feature) added test task.

--- backend/task/celery_task/tasks.py
+++ backend/task/celery_task/tasks.py
@@ -1,4 +1,5 @@
 import uuid
+from time import sleep
 
 from backend.task.celery import celery_app
 
@@ -8,3 +9,10 @@ def task_demo_async() -> str:
     uid = uuid.uuid4().hex
     print(f'Async task with {uid} success! *******************')
     return uid
+
+
+@celery_app.task(name='task_say')
+def task_say_async(msg: str) -> str:
+    sleep(5)
+    print(msg)
+    return msg
