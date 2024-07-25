737d410798e536050b97763106b30b99a637ace8
Ckala <agr.akyla@mail.ru>
2024-07-25 14:44:20 +0300

    (feature) added MongoDB connector.

---
+++ backend/core/mongo_db.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import Depends
+from pymongo import MongoClient
+
+from common.log import log
+from core.config import settings
+
+
+async def mongo_db() -> MongoClient:
+    client = MongoClient(settings.MONGO_URI)
+    db = client["dev"]
+    try:
+        yield db
+    except Exception as e:
+        log.error(e)
+    finally:
+        client.close()
+
+MongoDB = Annotated[MongoClient, Depends(mongo_db)]
