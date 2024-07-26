7624466bde381005066af1ad6c3a36c5d801dce8
Ckala <agr.akyla@mail.ru>
2024-07-26 13:54:11 +0300

    (fix) refactor mongo handlers.

--- backend/task/api/v1/handler.py
+++ backend/task/api/v1/handler.py
@@ -1,15 +1,16 @@
 from pathlib import Path
-from random import randint
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Body
+from fastapi import (
+    APIRouter
+)
 
-from api.mongo.schemas import UpdateDateMongoDBSchema
 from common.exception.errors import NotFoundError
-from common.response.response_chema import ResponseModel, response_base
+from common.response.response_chema import (
+    ResponseModel,
+    response_base
+)
 from common.response.response_code import CustomResponseCode
-from core.mongo_db import MongoDB
-from middleware.auth_jwt_middleware import JWTBearer
 from task.service.task_service import task_service
 
 router = APIRouter()
@@ -76,96 +77,3 @@ async def run_task(
     # task = task_service.run(name=name, args=args, kwargs={"msg": message})
     task = task_service.run(name=name, kwargs={"msg": message})
     return await response_base.success(data=task.task_id)
-
-
-@router.post(
-    '/',
-    summary='Добавить что то в MongoDB',
-    description="Создание записи в MongoDB",
-)
-async def create_in_mongo(db: MongoDB) -> ResponseModel:
-    user = db["users"].insert_one({
-        "_id": randint(1, 1000), # here primary key from Database (PostgresSQL / MySql)
-        "name": "admin",
-        "email": "admin@mail.ru",
-        "password": "<PASSWORD>",
-        "is_active": True,
-    })
-    result = db["users"].find_one(
-        {"_id": user.inserted_id}
-    )
-    return await response_base.success(
-        data=result
-    )
-
-
-@router.get(
-    '/{id}',
-    summary='Получить конкретную запись по id из MongoDB',
-    description="Получаем данные из MongoDB в виде коллекции",
-)
-async def get_by_id_from_mongo(id: int, db: MongoDB) -> ResponseModel:
-    user = db["users"].find_one({"_id": id})
-    if user is None:
-        return await response_base.fail(
-            res=CustomResponseCode.HTTP_404,
-            data=f"Not Found with id {id}"
-        )
-    return await response_base.success(
-        data=user
-    )
-
-
-@router.put(
-    '/{id}',
-    summary='Обновление данных по id MongoDB',
-    description="Обновляем данные в MongoDB и возвращаем результат обновленной модели",
-)
-async def update_by_id_from_mongo(
-    id: int,
-    db: MongoDB,
-    data: UpdateDateMongoDBSchema
-) -> ResponseModel:
-    data_dict = {k: v for k, v in data.dict().items() if v is not None}
-    if len(data_dict) >= 1:
-        update_result = db["users"].update_one(
-            {"_id": id}, {"$set": data_dict}
-        )
-
-        if update_result.modified_count == 0:
-            return await response_base.fail(
-                res=CustomResponseCode.HTTP_200,
-                data=f"Data with id {id} wasn't updated"
-            )
-    entity = db["users"].find_one({"_id": id})
-    if entity is None:
-        return await response_base.fail(
-            res=CustomResponseCode.HTTP_404,
-            data=f"Not Found with id {id}"
-        )
-    return await response_base.success(
-        data=entity
-    )
-
-
-@router.delete(
-    "/{id}",
-    summary="Удаление данных по id",
-    description="Удаление данных из MongoDB по id"
-)
-async def delete_by_id_from_mongo(
-    id: int,
-    db: MongoDB
-) -> ResponseModel:
-    delete_result = db["users"].delete_one({"_id": id})
-
-    if delete_result.deleted_count == 0:
-        return await response_base.fail(
-            res=CustomResponseCode.HTTP_404,
-            data=f"Entity with id {id} wasn't deleted"
-        )
-
-    return await response_base.success(
-        res=CustomResponseCode.HTTP_200,
-        data={"message": f"Entity with id {id} was deleted"}
-    )
