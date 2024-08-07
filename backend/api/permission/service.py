de070f0f90ce7d1c5fbbb6cf7f9cc62e9654e49c
Ckala <agr.akyla@mail.ru>
2024-08-07 12:36:05 +0300

    (refactor) improved permission service.py

--- backend/api/permission/service.py
+++ backend/api/permission/service.py
@@ -1,28 +1,37 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.permission.repository import permission_repository
-from api.permission.schemas import PermissionAllSchema, PermissionCreateSchema
+from api.permission.schemas import (
+    PermissionAllSchema,
+    PermissionCreateSchema,
+    PermissionSchema
+)
+from models.permission import Permission
 
 
 class PermissionService:
     @staticmethod
     async def get_permissions(db: AsyncSession):
         permissions = await permission_repository.get_permissions(db)
-        return [PermissionAllSchema(**permission) for permission in permissions]
+        # return [Permission(permission) for permission in permissions]
+        return [PermissionSchema(**permission.__dict__) for permission in permissions]
 
     @staticmethod
     async def get_permission_by_id(
-        db: AsyncSession,
-        permission_id: int
+            db: AsyncSession,
+            permission_id: int
     ):
         permission = await permission_repository.get_permission_by_id(permission_id, db)
         if permission is None:
             return None
-        return PermissionAllSchema(**permission)
+        return PermissionSchema(**permission.__dict__)
 
     @staticmethod
-    async def create_permission(db: AsyncSession):
-        pass
+    async def create_permission(data: dict, db: AsyncSession):
+        permission = await permission_repository.create_permission(data, db)
+        if permission is None:
+            return None
+        return PermissionSchema(**permission.__dict__)
 
     @staticmethod
     async def update_permission(db: AsyncSession):
