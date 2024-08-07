e0388f61628e623ca6829882cc272216018225ae
Ckala <agr.akyla@mail.ru>
2024-08-07 13:01:18 +0300

    (feature) added method delete permission in repository, service. also added new handler for delete permission.

--- backend/api/permission/handler.py
+++ backend/api/permission/handler.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload
 from starlette import status
 
-from api.permission.schemas import PermissionCreateSchema, PermissionAllSchema
+from api.permission.schemas import PermissionCreateSchema
 from api.permission.service import PermissionService
 from common.response.response_chema import ResponseModel, response_base
 from common.response.response_code import CustomResponseCode
@@ -62,8 +60,8 @@ async def get_permission_by_id(
     description="Create permission",
 )
 async def create_permission(
-    permission_create: PermissionCreateSchema,
-    db: AsyncSession = Depends(get_db)
+        permission_create: PermissionCreateSchema,
+        db: AsyncSession = Depends(get_db)
 ):
     permission = await PermissionService.create_permission(
         permission_create.model_dump(),
@@ -72,4 +70,25 @@ async def create_permission(
     return await response_base.success(
         res=CustomResponseCode.HTTP_201,
         data=permission
+    )
+
+
+@router.delete(
+    '/{permission_id}',
+    summary="Delete permission",
+    description="Delete permission",
+)
+async def delete_permission(
+        permission_id: int,
+        db: AsyncSession = Depends(get_db)
+) -> ResponseModel:
+    permission = await PermissionService.delete_permission(permission_id, db)
+    if permission is False:
+        return await response_base.fail(
+            res=CustomResponseCode.HTTP_404,
+            data=None
+        )
+    return await response_base.success(
+        res=CustomResponseCode.HTTP_204,
+        data=None
     )
