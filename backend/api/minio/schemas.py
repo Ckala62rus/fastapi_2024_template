32364eed259b1eedfa9db1bf938480c01e60f529
Ckala62rus <agr.akyla@mail.ru>
2024-11-05 20:32:23 +0300

    ( feature ) add minio schemas.

---
+++ backend/api/minio/schemas.py
@@ -0,0 +1,62 @@
+__all__ = [
+    'FileUrl',
+    'GetFile',
+]
+
+from common.schema import SchemaBase
+
+
+class FileBase(SchemaBase):
+    file: str
+    bucket: str
+
+
+class FileUrl(FileBase):
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "file": "car.jpg",
+                    "bucket": "images",
+                }
+            ]
+        }
+    }
+
+
+class GetFile(FileBase):
+    bucket: str | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "file": "car.jpg",
+                    "bucket": "images",
+                }
+            ]
+        }
+    }
+
+
+class FilesSchema(SchemaBase):
+    urls: list[str] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "code": 200,
+                    "msg": "OK",
+                    "data": {
+                        "urls": [
+                        "http://localhost:8001/api/v1/minio/file?file=71509ed8-01ce-445c-b16d-68ee41af436d-.jpg",
+                        "http://localhost:8001/api/v1/minio/file?file=71509ed8-01ce-445c-b16d-68ee41af436d-.jpg",
+                        "http://localhost:8001/api/v1/minio/file?file=71509ed8-01ce-445c-b16d-68ee41af436d-.jpg",
+                        "http://localhost:8001/api/v1/minio/file?file=71509ed8-01ce-445c-b16d-68ee41af436d-.jpg",
+                    ],
+                    }
+                }
+            ]
+        }
+    }
