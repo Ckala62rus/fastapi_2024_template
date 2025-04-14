f5fb573be4eee4af447c5d8f12a139408ec0f1c3
Ckala62rus <agr.akyla@mail.ru>
2025-04-14 20:44:22 +0300

    ( feature ) added db connector for tests and logger.

---
+++ backend/tests/utils/db.py
@@ -0,0 +1,51 @@
+import logging
+import sys
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy import create_engine, URL
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+
+from common.log import log
+from core.config import settings
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+def create_engine_and_session(url: str | URL):
+    logging.info("++++++++++++++++++++++++++++++++++++++")
+    print(settings.SQLALCHEMY_DATABASE_URL_FOR_ALEMBIC_TESTING)
+    logging.info("++++++++++++++++++++++++++++++++++++++")
+
+    try:
+        # Core database
+        engine = create_async_engine(url, echo=True, future=True, pool_pre_ping=True)
+        # log.success('success connect to database')
+    except Exception as e:
+        log.error('❌ Error to connect database {}', e)
+        sys.exit()
+    else:
+        db_session = async_sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+        return engine, db_session
+
+
+# async_engine, async_db_session = create_engine_and_session(settings.SQLALCHEMY_DATABASE_URL)
+async_engine, async_db_session = create_engine_and_session(settings.SQLALCHEMY_DATABASE_URL_FOR_ALEMBIC_TESTING)
+
+
+async def get_db_for_test() -> AsyncSession:
+    """session generator"""
+    session = async_db_session()
+    try:
+        yield session
+    except Exception as se:
+        await session.rollback()
+        raise se
+    finally:
+        await session.close()
+
+
+# Session Annotated
+CurrentSession = Annotated[AsyncSession, Depends(get_db_for_test)]
