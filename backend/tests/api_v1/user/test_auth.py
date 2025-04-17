e819d6d557375c175a30d4131d01a4304e3c0165
Ckala62rus <agr.akyla@mail.ru>
2025-04-17 19:46:57 +0300

    ( refactor ) added test.

--- backend/tests/api_v1/user/test_auth.py
+++ backend/tests/api_v1/user/test_auth.py
@@ -1,18 +1,10 @@
-import logging
-
 import httpx
 import pytest
-from fastapi import FastAPI
-from httpx import ASGITransport, AsyncClient
 from fastapi.testclient import TestClient
-import logging
+from common.log import logger
 
 from core.config import settings
-from tests.conftest import app, PYTEST_EMAIL, PYTEST_USERNAME
-
-logger  = logging.getLogger(__name__)
-logger.info("Logging test info message****************")
-logger.debug("Logging test debug message****************")
+from tests.conftest import app
 
 # Run test
 # pytest -v --pyargs tests
@@ -121,3 +113,31 @@ def test_me(
 
     assert response.status_code == httpx.codes.OK
     assert result["data"]["email"] == registration_data["email"]
+
+def test_get_all_users(
+    app: TestClient,
+) -> None:
+    registration_data = {
+        "email": "2admin@mail.ru",
+        "password": "123123",
+        "username": "2admin"
+    }
+    client = app
+    client.post(f'{settings.API_V1_STR}/user/registration', json=registration_data)
+
+    auth = client.post(f'{settings.API_V1_STR}/user/login', json={
+        "email": registration_data['email'],
+        "password": registration_data['password'],
+    })
+    auth_token = auth.json()['data']['access_token']
+
+    response_users = client.get(f'{settings.API_V1_STR}/user/users?page=1&limit=10',
+        headers={
+            'Authorization': f'Bearer {auth_token}'
+        }
+    )
+
+    users_count = len(response_users.json()['data'])
+
+    assert response_users.status_code == httpx.codes.OK
+    assert users_count == 1
