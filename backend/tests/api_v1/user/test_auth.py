56edb55655eb889664e7bbc0dc029d584b189575
Ckala62rus <agr.akyla@mail.ru>
2025-04-22 20:34:36 +0300

    ( refactor ) improved tests.

--- backend/tests/api_v1/user/test_auth.py
+++ backend/tests/api_v1/user/test_auth.py
@@ -10,76 +10,33 @@ from tests.conftest import app
 # pytest -v --pyargs tests
 # pytest -v --pyargs tests --capture=no
 
-
-# @pytest.mark.asyncio
-# async def test_me(
-#     # client: TestClient,
-#     token_headers: dict[str, str]
-# ) -> None:
-#     async with AsyncClient(
-#         transport=ASGITransport(app=app), base_url="http://test"
-#     ) as async_client:
-#         response = await async_client.get(f'{settings.API_V1_STR}/user/me', headers= token_headers)
-#     assert response.status_code == 200
-#     assert response.json()['code'] == 200
-#     assert response.json()['data']['email'] == PYTEST_EMAIL
-#     assert response.json()['data']['username'] == PYTEST_USERNAME
-
-# @pytest.mark.asyncio
-# async def test_registration(
-#     # async_client: AsyncClient
-#         app: FastAPI,
-#         log: logging.Logger
-# ) -> None:
-#     data = {
-#         "email": "2admin@mail.ru",
-#         "password": "123123",
-#         "username": "2admin"
-#     }
-#     assert True == True
-#
-#     log.info('Logging test info message')
-#     async with AsyncClient(
-#             transport=ASGITransport(app=app), base_url="http://test"
-#     ) as async_client:
-#         response = await async_client.post(f'{settings.API_V1_STR}/user/registration', json=data)
-#         log.info(response)
-#     # response = await async_client.post(f'{settings.API_V1_STR}/user/registration', json=data)
-#     assert response.status_code == 200
-#     print(response.status_code)
-#     logger.info("++++++++++++++++++++++++++++++++++++++")
-
-
+@pytest.mark.asyncio
 def test_registration_sync(
-    app: FastAPI,
-    # log: logging.Logger
+    app: TestClient,
 ) -> None:
     data = {
         "email": "2admin@mail.ru",
         "password": "123123",
         "username": "2admin"
     }
-    client = TestClient(app)
-    response = client.post(f'{settings.API_V1_STR}/user/registration', json=data)
-
+    response = app.post(f'{settings.API_V1_STR}/user/registration', json=data)
+    logger.debug(response.json())
     assert response.status_code == 200
     assert response.json()['code'] == 201
 
 def test_login(
-    app: FastAPI,
-    # log: logging.Logger
+    app: TestClient,
 ) -> None:
     registration_data = {
         "email": "2admin@mail.ru",
         "password": "123123",
         "username": "2admin"
     }
-    client = TestClient(app)
+    client = app
     client.post(f'{settings.API_V1_STR}/user/registration', json=registration_data)
     response = client.post(f'{settings.API_V1_STR}/user/login', json={
         "email": registration_data['email'],
         "password": registration_data['password'],
-        # "password": 'qwerty',
     })
 
     result = response.json()
